4308038c642434b265c44ff63a085b1eb0d722a7
Griffin Maxwell <griffin@maxwell-family.net>
2018-06-02 12:58:17 -0400

    add in I_Error handler

--- source/Lexer_Concrete.c
+++ source/Lexer_Concrete.c
@@ -3,13 +3,18 @@
  */
 
 #include "Lexer_Concrete.h"
+#include "util.h"
 
 static void lex(I_Lexer_t *interface, const char *source, I_List_t *tokens)
 {
+   REINTERPRET(instance, interface, Lexer_Concrete_t *);
 
+   Error_Report(instance->errorHandler, "Lexer doesn't do anything yet.");
 }
 
-void Lexer_Concrete_Init(Lexer_Concrete_t *instance)
+void Lexer_Concrete_Init(Lexer_Concrete_t *instance, I_Error_t *errorHandler)
 {
    instance->interface.lex = &lex;
+
+   instance->errorHandler = errorHandler;
 }
--- source/Lexer_Concrete.h
+++ source/Lexer_Concrete.h
@@ -7,15 +7,18 @@
 #define _LEXER_CONCRETE_H
 
 #include "I_Lexer.h"
+#include "I_Error.h"
 
 typedef struct
 {
    I_Lexer_t interface;
+
+   I_Error_t *errorHandler;
 } Lexer_Concrete_t;
 
 /*
  * Initialize a Lexer_Concrete.
  */
-void Lexer_Concrete_Init(Lexer_Concrete_t *instance);
+void Lexer_Concrete_Init(Lexer_Concrete_t *instance, I_Error_t *errorHandler);
 
 #endif
